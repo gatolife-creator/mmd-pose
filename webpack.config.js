604f41a5625fd8de4c96f207bc8975e85161ece8
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-12-03 23:40:55 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "development",
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: `./src/sketch.js`,
+
+  // ファイルの出力設定
+  output: {
+    // 出力ファイル名
+    filename: "main.js",
+  },
+  devServer: {
+    static: "dist",
+    open: true,
+  },
+};
